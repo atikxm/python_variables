db72596462d6bdd1acee28210d369127a5cb402f
ATIKUR RAHMAN <atikurrahmanxm@gmail.com>
2025-06-02 20:14:59 +0530

    learning_python_variable

---
+++ variable.py
@@ -0,0 +1,57 @@
+#name="alien"
+#age=22
+#height=1.75
+
+
+#Print Variable & Type
+'''''
+name="alien"
+age=22
+print(name)
+print(age)
+print(type(name))
+'''
+
+#Variable declare korp
+
+'''amy_name = "alien"
+my_age = 22
+my_height = 1.75
+is_student = True
+
+print(my_name)
+print(my_age)
+print(my_height)
+print(is_student)
+# Print Type    
+print(type(my_name))
+print(type(my_age))
+print(type(my_height))
+print(type(is_student)) 
+'''
+#input variable
+
+'''name = input("Tumar naam ki? ")
+print("Hello", name)
+'''
+'''
+num1=input("Tumar prothom number ta ki: ")
+num2=input("Tumar ditiyo number ta ki:")  
+
+num1=int(num1)
+num2=int(num2)
+
+sum=num1+num2
+print("tomar namer jogfol holo : ",sum)
+'''
+'''
+name=input("Enter your name : ")
+age=input("Enter your age : ")
+
+print ("Your name is : ", name)
+print("Your agfe is : ",age)
+'''
+num1=input("Enter first number : ")
+num2=input("Enter second number : ")
+sum=int(num1)+int(num2)
+print("The sum of the two numbers is : ", sum)
